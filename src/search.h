d70321bd2b99d9902ebd21605980c345593b2e78
Jacky Royer <kiky270281@gmail.com>
2025-11-25 14:36:28 +0100

    feat: add QuickSort & Binary Search

---
+++ src/search.h
@@ -0,0 +1,9 @@
+﻿#ifndef SEARCH_H
+#define SEARCH_H
+
+/* Fonction de recherche binaire.
+- cherche 'target' (la valeur à trouver) dans le tableau 'arr'.
+Le tableau DOIT être trié pour que cette fonction fonctionne. */
+int binary_search(int arr[], int low, int high, int target);
+
+#endif // SEARCH_H
